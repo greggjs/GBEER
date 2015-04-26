c34e28404c1ff7033267f46018c89663332fd4d4
Jake Gregg <greggjs@miamioh.edu>
2015-04-26 16:55:50 -0400

    Fixed pathing in the tasks

--- tasks.py
+++ tasks.py
@@ -23,30 +23,30 @@ def add(x, y):
 @app.task
 def createJob(request_id):
     # Create the phylo tree for request
-    create_newick_tree.get_newick_tree('./queries/{0}/genomes'.format(str(request_id)),
-        './queries/{0}/out_tree.nwk'.format(str(request_id)),
-        './queries/{0}/phylo_tree.txt'.format(str(request_id)), str(request_id))
+    create_newick_tree.get_newick_tree(os.path.join(settings.QUERY_FOLDER, '{0}/genomes'.format(str(request_id))),
+        os.path.join(settings.QUERY_FOLDER, '{0}/out_tree.nwk'.format(str(request_id))),
+        os.path.join(settings.QUERY_FOLDER, '{0}/phylo_tree.txt'.format(str(request_id))), str(request_id))
 
     # Do operon comparisons on request
-    event_cmd = 'python gene_block_evolution/make_event_distance_matrix.py -i {0} -I {1} -o {2} -F {3}'.format(
+    event_cmd = 'python /vagrant/GBEER/gene_block_evolution/make_event_distance_matrix.py -i {0} -I {1} -o {2} -F {3}'.format(
         settings.OPERON_DICT, settings.OPERON_INFOLDER,
-        './queries/{0}'.format(str(request_id)),
-        './queries/{0}/filter_file'.format(str(request_id))
+        os.path.join(settings.QUERY_FOLDER, '{0}'.format(str(request_id))),
+        os.path.join(settings.QUERY_FOLDER, '{0}/filter_file'.format(str(request_id)))
     )
     os.system(event_cmd)
 
     # Normalize results of request
-    matrix_cmd = 'python gene_block_evolution/get_output_probs.py -i {0}'.format(
-        './queries/{0}/event_dict.p'.format(str(request_id))
+    matrix_cmd = 'python /vagrant/GBEER/gene_block_evolution/get_output_probs.py -i {0}'.format(
+        os.path.join(settings.QUERY_FOLDER, '{0}/event_dict.p'.format(str(request_id)))
     )
     os.system(matrix_cmd)
 
     operonVisualUpdate.create_operon_images(
         settings.OPERON_INFOLDER,
-        './queries/{0}/asma_outlist.txt'.format(str(request_id)),
-        './queries/{0}/out_tree.nwk'.format(str(request_id)),
-        './queries/{0}/event_dict.p'.format(str(request_id)),
-        './queries/{0}/output'.format(str(request_id)),
+        os.path.join(settings.QUERY_FOLDER, '{0}/asma_outlist.txt'.format(str(request_id))),
+        os.path.join(settings.QUERY_FOLDER, '{0}/out_tree.nwk'.format(str(request_id))),
+        os.path.join(settings.QUERY_FOLDER, '{0}/event_dict.p'.format(str(request_id))),
+        os.path.join(settings.QUERY_FOLDER, '{0}/output'.format(str(request_id))),
         request_id
     )
 
